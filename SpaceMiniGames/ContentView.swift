896771d8d01de9658f987d91b0f5609cc357a991
Ilya Lysenko <ilyalysenko7828@gmail.com>
2025-10-07 13:29:11 +0300

    Первый комит для проверки работы

---
+++ SpaceMiniGames/ContentView.swift
@@ -0,0 +1,7 @@
+//
+//  ContentView.swift
+//  SpaceMiniGames
+//
+//  Created by Илья Лысенко on 07.10.2025.
+//
+
---
+++ SpaceMiniGames/SpaceMiniGamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceMiniGamesApp.swift
+//  SpaceMiniGames
+//
+//  Created by Илья Лысенко on 07.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct SpaceMiniGamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
